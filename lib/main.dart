608cecf55deb1ec2d19e17c785f56e60458b529f
Ali Çimen <alicimen0614@gmail.com>
2024-12-10 17:20:55 +0300

    implement auto_router

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:asset_tracker/core/router/app_router.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,14 +8,15 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'AssetTracker',
-        theme: ThemeData(
-          useMaterial3: true,
-        ),
-        home: Container());
+    return MaterialApp.router(
+      title: 'AssetTracker',
+      theme: ThemeData(
+        useMaterial3: true,
+      ),
+      routerDelegate: router.delegate(),
+      routeInformationParser: router.defaultRouteParser(),
+    );
   }
 }
---
+++ lib/presentation/views/login/login_view.dart
@@ -0,0 +1,14 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+
+@RoutePage()
+class LoginView extends StatelessWidget {
+  const LoginView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.black,
+    );
+  }
+}
