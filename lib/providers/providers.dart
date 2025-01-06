0510fa597b58a8654684c76dede76be0ae042984
Ali Çimen <alicimen0614@gmail.com>
2025-01-06 11:45:22 +0300

    Refactor: simplify authentication service initialization in providers

--- lib/providers/providers.dart
+++ lib/providers/providers.dart
@@ -1,16 +1,12 @@
 import 'package:asset_tracker/core/models/auth_state_model.dart';
-import 'package:asset_tracker/core/services/auth_service.dart';
 import 'package:asset_tracker/core/state/auth_notifier.dart';
 import 'package:asset_tracker/data/services/firebase_auth_service.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final authServiceProvider = Provider<AuthService>((ref) {
-  return FirebaseAuthService(FirebaseAuth.instance);
-});
+final _firebaseAuthService = FirebaseAuthService(FirebaseAuth.instance);
 
 final authNotifierProvider =
     StateNotifierProvider<AuthNotifier, AuthState>((ref) {
-  final authService = ref.watch(authServiceProvider);
-  return AuthNotifier(authService);
+  return AuthNotifier(_firebaseAuthService);
 });
